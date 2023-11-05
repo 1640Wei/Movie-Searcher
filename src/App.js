aec11af57b4cb730a8255f02f3d68fc84b3f74d2
Ching Wei Lai <HTY140226@gmail.com>
2023-11-05 07:32:26 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+//API key:359ac4a5
+
+const API_URL = "http://www.omdbapi.com?apikey=359ac4a5";
 
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect (() => {
+    searchMovies("Superman");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>App</h1>
     </div>
   );
 }
